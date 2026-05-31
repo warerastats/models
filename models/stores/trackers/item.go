f78bb2f56aeb5cb69e39c69e69612ad733589a82
Aaron Jonk <AJ@jonk.name>
2026-06-01 01:00:58 +0200

    Incorrect order

--- models/stores/trackers/item.go
+++ models/stores/trackers/item.go
@@ -59,7 +59,7 @@ func (s *ItemStore) ensureIndex(ctx context.Context) {
 
 	_, err = s.coll.Indexes().CreateOne(ctx, mongo.IndexModel{
 		Keys: bson.D{
-			{Key: "_id", Value: -1},
+			{Key: "_id", Value: 1},
 		},
 	})
 	if err != nil {
